0fa3eb0fa2be67e71ed3911d9965637e29b151b4
Breck Cowan <breckcowan@gmail.com>
2021-08-29 17:57:31 -0500

    init commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const replaceTemplate = require('./modules/replaceTemplate')
 ////////////////////////////////////////////////////////////////////////
 // FILES
 ///////////////////////////////////////////////////////////////////////
@@ -36,19 +36,7 @@ const url = require('url');
 // Synchronous top level blocking code, will be read once
 // and then not will not matter that it is blocking
 // template files
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCENAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-// Creating the case for if something in not organic, how it should be handled
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-}
+
 // Defining the dir path for the templates
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
@@ -59,24 +47,32 @@ const dataObj = JSON.parse(data);
 // end blocking code
 // Creating server
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+
+  const {
+    query,
+    pathname
+  } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
       'Content-type': 'text/html'
     });
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-
     res.end(output);
 
     // Products page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json'
     });
